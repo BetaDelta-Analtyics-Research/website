d5754d9fca7dc77bff87e42b2321e47aaff08637
Brian Davidhizar <brian.davidhizar@gmail.com>
2022-12-17 09:09:35 -0600

    2022.12.17 commit

--- SP500_40.R
+++ SP500_40.R
@@ -1,39 +1,9 @@
----
-title: "Portfolio"
-format:
-  html:
-    theme: lux
-    page-layout: full
-    margin-header: |
-     [GET A CHART](https://share.hsforms.com/1I8JrOQTdS2SPY59SMnyfNge0x16)
----
-
-##### Chart better. Lead better.
-***
-
-* Don't waste hours teaching yourself how to deliver effective data visualizations.
-
-* Position yourself among the professionals in your sphere.
-
-* You don't need deep pockets to make a great impression or cause real impact.
-
-* [Connect with us and get a chart now!](https://share.hsforms.com/1I8JrOQTdS2SPY59SMnyfNge0x16)
-
-***
-
-Collaboration is easy. We leverage Google Drive, Microsoft Teams, and GitHub.
-
-Connect with us on social.
-
-<br>
-
-```{r, warning=FALSE, message=FALSE, echo=FALSE, fig.height=5.5, fig.width=7.5, dev = "png", dpi=100}
+library(tidyquant)
 library(tidyverse)
 library(readxl)
 library(ggrepel)
 library(geomtextpath)
-library(tidyquant)
-library(tidyverse)
+library(gridExtra)
 
 colorp <- c("#252525", "#969696", "#a50f15")
 
@@ -46,6 +16,8 @@ sp500_monthly <- sp500_prices %>%
 sp500_returns <- tq_get("^GSPC", get  = "stock.prices", from = "1928-01-01", to   = "2022-11-30") %>%
   tq_transmute(adjusted, periodReturn, period = "monthly", col_rename = "sp500.returns")
 
+class(sp500_returns)
+
 sp500_xts <- xts(sp500_returns$sp500.returns, order.by=sp500_returns$date)
 
 sp500_40 <- rollapply(sp500_xts, width = 480, FUN = Return.annualized, fill = 0) 
@@ -53,13 +25,13 @@ names(sp500_40) <- "S&P 500"
 sp500_40 <- sp500_40[480:length(sp500_40)]
 
 
-ggplot(sp500_40, aes(x=index(sp500_40), y=sp500_40$`S&P 500`*100)) + 
+plot1 <- ggplot(sp500_40, aes(x=index(sp500_40), y=sp500_40$`S&P 500`*100)) + 
   geom_line() +
   geom_hline(yintercept=7, linetype=2, color=colorp[3]) +
   geom_text(aes(index(sp500_40)[1],7),label = '7% growth', vjust = -1, hjust=.3, color=colorp[3], family='serif', size=4) +
   geom_hline(yintercept=9, linetype=2, color=colorp[3]) +
   geom_text(aes(index(sp500_40)[1],9),label = '9% growth', vjust = -1, hjust=.3, color=colorp[3], family='serif', size=4) +
-  labs(title = "S&P 500 Frequently Achieved 7% Annualized Growth Over 40-Year Periods",
+  labs(title = "S&P 500 over 40-Years Frequently Achieved 7% Annualized Growth, Broke 9%",
        subtitle = "S&P 500 Rolling 40-Year Returns Annualized",
        caption = "Data visaulization by BetaDelta Analytics & Research
                   \n Source: Yahoo Finance Historical Data: Adjusted Prices for ^GSPC
@@ -73,15 +45,13 @@ ggplot(sp500_40, aes(x=index(sp500_40), y=sp500_40$`S&P 500`*100)) +
         axis.title.x=element_text(size=14, hjust = 0),
         axis.title.y=element_blank(), #element_text(size=14),
         axis.text.y = element_text(size=10, hjust = 0), #element_blank(),  #remove y axis labels
-        axis.ticks.y= element_blank(),
+        #axis.ticks.y= element_blank(),
         axis.line.y = element_blank(),
         text=element_text(family = "serif") #remove y axis ticks
   )
-```
+  
 
-***
-
-```{r, warning=FALSE, message=FALSE, echo=FALSE, fig.height=5.5, fig.width=7.5, dev = "png", dpi=100}
+##############################################################################################
 
 df <- read_xlsx("Growth of 10 Dollars Table Seven.xlsx")
 
@@ -89,20 +59,20 @@ df_points <- rbind(head(df,1), df[120,], df[240,], df[360,], tail(df,1))
 
 colorp <- c("#252525", "#969696", "#a50f15")
 
-ggplot(df, aes(date, value)) + 
+plot2 <- ggplot(df, aes(date, value)) + 
   geom_textline(label = "$10  a day invested monthly @ 7% annual growth.", 
                 linewidth = 1, color = colorp[1], vjust = 1.5, hjust = 0.75, family = "serif", size = 5) +
-  geom_point(data = df_points, aes(date, value), size = 4, color = colorp[3], family = "serif") +
+  geom_point(data = df_points, aes(date, value), size = 4, color = colorp[3]) +
   geom_text_repel(data = df_points, aes(label = scales::dollar(round(value,0))), 
-            size = 4, color = colorp[1], direction="both", vjust = -1.5, hjust = 1.2, point.padding = 10, 
-            family = "serif") + 
+                  size = 4, color = colorp[1], direction="both", vjust = -1.5, hjust = 1.2, point.padding = 10, 
+                  family = "serif") + 
   labs(title = "What habit can you change to save $10 a day?",
-       subtitle = "Financial coaching can help you change your life. \n7% annual return on investment is no guarantee, but totally doable.",
+       subtitle = "Financial coaching can help you change your life.",
        caption = "\n Data visaulization by BetaDelta Analytics & Research") + 
   xlab(" \n 40 years starting New Year's 2023.  You can do it.") +
   ylab("") +
   theme_classic() +
-  theme(plot.title=element_text(size=16),
+  theme(plot.title=element_text(size=20),
         plot.caption=element_text(size=8, color=colorp[1]),
         axis.title.x=element_text(size=14, hjust = 0),
         axis.title.y=element_blank(), #element_text(size=14),
@@ -110,6 +80,6 @@ ggplot(df, aes(date, value)) +
         axis.ticks.y=element_blank(),
         axis.line.y = element_blank(),
         text=element_text(family = "serif") #remove y axis ticks
-        )
-```
+  )
 
+grid.arrange(plot1, plot2, nrow = 2)
